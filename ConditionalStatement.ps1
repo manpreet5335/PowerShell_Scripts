506799cbd0356b79585ba80a7687657207800c0d
Manpreet Kaur <manpreet.kaur2@sheridancollege.ca>
2026-01-16 12:49:08 -0500

    Update ConditionalStatement.ps1

--- ConditionalStatement.ps1
+++ ConditionalStatement.ps1
@@ -4,4 +4,4 @@ if ($x -gt 10) {Write-Host "This is if statement"
 $x--}
 else {Write-Host "This is else statement"
 $x--}
-Write-host "this is a test"
+Write-host "this is a test It is interesting to see changes being displayed"
